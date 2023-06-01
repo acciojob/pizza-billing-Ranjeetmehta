88e5af15b1846a1a7aa19028c35438f64a9a90d0
ranjeetmehta <ranjeetkumarmehta14418@gmail.com>
2023-06-01 18:32:18 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,11 +8,22 @@ public class Main {
     dp.addExtraCheese();
     dp.addTakeaway();
     System.out.println(dp.getBill());
+    DeluxePizza dp1=new DeluxePizza(false);
+    System.out.println(dp1.getPrice());
+    dp1.addTakeaway();
+    dp1.addExtraToppings();
+    dp1.addTakeaway();
+    System.out.println(dp1.getBill());
 
     Pizza p = new Pizza(true);
-    p.addExtraCheese();
+    System.out.println(p.getPrice());
+//    p.addExtraCheese();
     p.addExtraToppings();
     p.addTakeaway();
     System.out.println(p.getBill());
+    Pizza p1=new Pizza(false);
+    System.out.println(p1.getPrice());
+    p1.addTakeaway();
+    System.out.println(p1.getBill());
   }
 }
